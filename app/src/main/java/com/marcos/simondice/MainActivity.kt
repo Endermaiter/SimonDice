b5ccf28ccc23f5edb5e13854eac815084b31c5e9
Endermaiter <mpereznovoa@danielcastelao.org>
2022-11-07 10:45:24 +0100

    Interfaz acabada y estructura definida

--- app/src/main/java/com/marcos/simondice/MainActivity.kt
+++ app/src/main/java/com/marcos/simondice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.marcos.simondice
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,57 +10,82 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var secuencia: ArrayList<String> = TODO()
-    val ronda: Int = secuencia.size
-    val record:Int
-    val colorSeleccionado:String
+    var secuencia: ArrayList<Int> = arrayListOf()
+    var ronda: Int = 0
+    var record: Int = 0
+
+    lateinit var buttonStart: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ButtonStart: Button = findViewById(R.id.buttonStart)
-        ButtonStart.setOnClickListener{
+        buttonStart = findViewById(R.id.start_button)
+        buttonStart.setOnClickListener {
             iniciarPartida()
+            generarSecuencia()
         }
 
     }
 
-    private fun iniciarPartida(){
+    private fun iniciarPartida() {
         Log.d("JUEGO", "PARTIDA: Comienza la partida")
-        Log.d("JUEGO","PARTIDA: Ronda = $ronda")
-        Log.d("JUEGO", "SECUENCIA: Comienza la secuencia")
-        Log.d("JUEGO", "SECUENCIA: Termina la secuencia")
-
-        Log.d("JUEGO", "PARTIDA: Esperando al usuario")
-        Log.d("JUEGO", "PARTIDA: El jugador ha pulsado la secuencia de colores")
-
-        Log.d("JUEGO", "SECUENCIA: Comprobando secuencia de colores correcta...")
-        Log.d("JUEGO","SECUENCIA: Color incorrecto --> GAME OVER")
-        Log.d("JUEGO","SECUENCIA: Color correcto. Saltando al siguiente...")
-
-        Log.d("JUEGO","PARTIDA: Todos los colores son correctos. Pasando al siguiente nivel...")
-
+        Log.d("JUEGO", "PARTIDA: Ronda = $ronda")
+        // TODO inicializar array de secuencia
+        ronda++
     }
 
-    private fun colorRandom() {
-        val numeroRandom = Random().nextInt(4)+1
+    private fun generarSecuencia(){
+        Log.d("JUEGO","Genero secuencia y muestro " + ronda.toString())
+        var i = 0
+        while (i < ronda) {
+            Log.d("JUEGO", "SECUENCIA: Comienza la secuencia")
+            mostrarSecuencia()
+            i++
+            Log.d("JUEGO", "SECUENCIA: Termina la secuencia")
+        }
+    }
 
-        Log.d("JUEGO", "COLOR: Nuevo color random generado")
+    private fun mostrarSecuencia() {
+        val numeroRandom = Random().nextInt(4) + 1
 
-        val ButtonY: ImageButton = findViewById(R.id.buttonY)
+        Log.d("JUEGO", "COLOR: Nuevo color random generado " + numeroRandom.toString())
+        secuencia.add(numeroRandom)
+        /*val ButtonY: ImageButton = findViewById(R.id.buttonY)
         val ButtonG: ImageButton = findViewById(R.id.buttonG)
         val ButtonB: ImageButton = findViewById(R.id.buttonB)
-        val ButtonR: ImageButton = findViewById(R.id.buttonR)
+        val ButtonR: ImageButton = findViewById(R.id.buttonR)*/
 
-        val buttonColor = when (numeroRandom) {
-            1 -> ButtonY.setImageResource(R.mipmap.yellowLight_button_foreground)
-            2 -> ButtonG.setImageResource(R.mipmap.greenLight_button_foreground)
-            3 -> ButtonB.setImageResource(R.mipmap.blueLight_button_foreground)
-            else -> {ButtonR.setImageResource(R.mipmap.redLight_button_foreground)}
-        }
+        Log.d("JUEGO", "Generado: " + numeroRandom.toString() )
+
+        /*when (numeroRandom) {
+            1 -> ButtonY.setImageResource(R.mipmap.yellowlight_button_foreground)
+            2 -> ButtonG.setImageResource(R.mipmap.greenlight_button_foreground)
+            3 -> ButtonB.setImageResource(R.mipmap.bluelight_button_foreground)
+            else -> {
+                ButtonR.setImageResource(R.mipmap.redlight_button_foreground)
+            }
+        }*/
+
+
+       /* ButtonY.setImageResource(R.mipmap.yellow_button_foreground)
+        ButtonG.setImageResource(R.mipmap.green_button_foreground)
+        ButtonB.setImageResource(R.mipmap.blue_button_foreground)
+        ButtonR.setImageResource(R.mipmap.red_button_foreground)*/
 
         Log.d("JUEGO", "COLOR: Nuevo color visualizado al jugador")
+
+    }
+
+    private fun aguardaPosicion() {
+
+    }
+
+    private fun compruebaColor() {
+
+    }
+
+    private fun subirNivel() {
 
     }
 }
