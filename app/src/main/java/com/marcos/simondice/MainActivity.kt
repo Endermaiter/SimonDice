09ed5d680e3cf0ad4b1232e05ed2ca837ad314e0
Endermaiter <mpereznovoa@danielcastelao.org>
2022-11-07 11:14:31 +0100

    Implementacion Coroutina (Necesita revision)

--- app/src/main/java/com/marcos/simondice/MainActivity.kt
+++ app/src/main/java/com/marcos/simondice/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.marcos.simondice
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import java.util.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,27 +52,32 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("JUEGO", "COLOR: Nuevo color random generado " + numeroRandom.toString())
         secuencia.add(numeroRandom)
-        /*val ButtonY: ImageButton = findViewById(R.id.buttonY)
+        val ButtonY: ImageButton = findViewById(R.id.buttonY)
         val ButtonG: ImageButton = findViewById(R.id.buttonG)
         val ButtonB: ImageButton = findViewById(R.id.buttonB)
-        val ButtonR: ImageButton = findViewById(R.id.buttonR)*/
+        val ButtonR: ImageButton = findViewById(R.id.buttonR)
 
         Log.d("JUEGO", "Generado: " + numeroRandom.toString() )
 
-        /*when (numeroRandom) {
+        when (numeroRandom) {
             1 -> ButtonY.setImageResource(R.mipmap.yellowlight_button_foreground)
             2 -> ButtonG.setImageResource(R.mipmap.greenlight_button_foreground)
             3 -> ButtonB.setImageResource(R.mipmap.bluelight_button_foreground)
             else -> {
                 ButtonR.setImageResource(R.mipmap.redlight_button_foreground)
             }
-        }*/
+        }
 
+        var corutina: Job? = null
+        corutina = GlobalScope.launch(Dispatchers.Main){
+                Log.d("JUEGO", "Coroutina" + corutina.toString())
+                delay(1000L)
+        }
 
-       /* ButtonY.setImageResource(R.mipmap.yellow_button_foreground)
+        ButtonY.setImageResource(R.mipmap.yellow_button_foreground)
         ButtonG.setImageResource(R.mipmap.green_button_foreground)
         ButtonB.setImageResource(R.mipmap.blue_button_foreground)
-        ButtonR.setImageResource(R.mipmap.red_button_foreground)*/
+        ButtonR.setImageResource(R.mipmap.red_button_foreground)
 
         Log.d("JUEGO", "COLOR: Nuevo color visualizado al jugador")
 
